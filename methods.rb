3d5f44c0b487232f41f4a484c1024c0f6d3b69ec
jesse <jesse.kibaara@student.moringaschool.com>
2023-03-10 21:31:08 +0300

    greet

--- methods.rb
+++ methods.rb
@@ -7,16 +7,3 @@ def greet_programmer
     puts "Hello, #{name}!"
   end
   
-  def greet_with_default(name = "programmer")
-    puts "Hello, #{name}!"
-  end
-  
-  def add(num1, num2)
-    num1 + num2
-  end
-  
-  def halve(num)
-    return num / 2 if num.is_a? Integer
-    nil
-  end
-  
